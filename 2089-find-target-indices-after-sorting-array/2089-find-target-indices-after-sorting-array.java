53197a237812f22ca712d82d02d08542c99944c9
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-01-26 08:24:51 +0530

    Time: 1 ms (97.32%), Space: 43.9 MB (23.90%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,11 +1,20 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer>ans=new ArrayList();
-
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target)ans.add(i);
-        }
-        return ans;
+       List<Integer>ans=new ArrayList();
+       int count=0,lessthan=0;
+       for(int num:nums){
+           if(num==target)count++;
+           if(num<target)lessthan++;
+       }
+       for(int i=0;i<count;i++){
+           ans.add(lessthan++);
+       }
+       return ans;
+        // List<Integer>ans=new ArrayList();
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==target)ans.add(i);
+        // }
+        // return ans;
     }
 }
