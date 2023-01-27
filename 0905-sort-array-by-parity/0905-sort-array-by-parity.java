85f226f505723401b9343c3b37625fccb74e4832
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-01-27 15:56:15 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (89.27%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,23 +1,31 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        
-        List<Integer>even=new ArrayList();
-        List<Integer>odd=new ArrayList();
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                even.add(nums[i]);
-            }else{
-                odd.add(nums[i]);
+        for(int i=0,j=0;j<nums.length;j++){
+            if(nums[j]%2==0){
+                int temp=nums[i];
+                nums[i++]=nums[j];
+                nums[j]=temp;
             }
         }
-        int k=0;
-        for(int i=0;i<even.size();i++){
-            nums[k++]=even.get(i);
-        }
-        for(int i=0;i<odd.size();i++){
-            nums[k++]=odd.get(i);
-        }
         return nums;
+        // List<Integer>even=new ArrayList();
+        // List<Integer>odd=new ArrayList();
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]%2==0){
+        //         even.add(nums[i]);
+        //     }else{
+        //         odd.add(nums[i]);
+        //     }
+        // }
+        // int k=0;
+        // for(int i=0;i<even.size();i++){
+        //     nums[k++]=even.get(i);
+        // }
+        // for(int i=0;i<odd.size();i++){
+        //     nums[k++]=odd.get(i);
+        // }
+        // return nums;
     }
 }
