8e983e94f87f51b715179c93d53d1ea6d9b5e817
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-01-27 16:02:36 +0530

    Added solution - LeetHub

---
+++ Total Decoding Messages - GFG/total-decoding-messages.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.CountWays(str);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int CountWays(String str)
+    {
+        // code here
+      int n = str.length();
+        if(str.charAt(0)=='0') return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.parseInt(str.substring(i-1, i));
+            int second = Integer.parseInt(str.substring(i-2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
+            dp[i] %= (int)(Math.pow(10, 9) + 7);
+            if(dp[i]==0) return 0;
+        }
+        return dp[n];
+    }
+}
