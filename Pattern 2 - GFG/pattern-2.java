294436356a8263b2cbe21d7f886abbd37ab2dff6
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-01-29 17:36:14 +0530

    Added solution - LeetHub

--- Pattern 2 - GFG/pattern-2.java
+++ Pattern 2 - GFG/pattern-2.java
@@ -24,7 +24,7 @@ class Solution {
         // code here
         for(int i=0;i<n;i++){
             for(int j=0;j<i+1;j++){
-                System.out.print("*"+" ");
+                System.out.print("* ");
             }
             System.out.println();
         }
