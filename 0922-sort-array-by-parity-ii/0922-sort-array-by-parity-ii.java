da9a8f6365dfc4d2e4036dd6e06beb58cf3e2ad8
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-02-01 21:19:53 +0530

    Time: 2 ms (99.45%), Space: 44.9 MB (55.05%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         
-        List<Integer>even=new ArrayList();
-        List<Integer>odd=new ArrayList();
+        int i=0,j=1;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                even.add(nums[i]);
-            }else{
-                odd.add(nums[i]);
+        while(i<nums.length && j<nums.length){
+            if(nums[i]%2==0)i+=2;
+            else{
+                if(nums[j]%2!=0)j+=2;
+                else{
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
             }
         }
-        int k=0;
-        for(int i=0;i<nums.length;i+=2){
-            nums[i]=even.get(k++);
-        }
-        int k2=0;
-        for(int i=1;i<nums.length;i+=2){
-            nums[i]=odd.get(k2++);
-        }
         return nums;
+        // List<Integer>even=new ArrayList();
+        // List<Integer>odd=new ArrayList();
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]%2==0){
+        //         even.add(nums[i]);
+        //     }else{
+        //         odd.add(nums[i]);
+        //     }
+        // }
+        // int k=0;
+        // for(int i=0;i<nums.length;i+=2){
+        //     nums[i]=even.get(k++);
+        // }
+        // int k2=0;
+        // for(int i=1;i<nums.length;i+=2){
+        //     nums[i]=odd.get(k2++);
+        // }
+        // return nums;
     }
 }
