dd482e115dd5f4595540fb4a762eebea8e43bbc0
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-02-03 11:27:56 +0530

    Time: 6 ms (36.06%), Space: 42 MB (54.85%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,14 +1,35 @@
 class Solution {
     public int findLucky(int[] arr) {
         HashMap<Integer,Integer>map=new HashMap();
-        Arrays.sort(arr);
+        
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
        System.out.print(map);
-       for(int i=arr.length-1;i>=0;i--){
-           if(arr[i]==map.get(arr[i]))return arr[i];
+       int max_key=Integer.MIN_VALUE;
+       int returning=0;
+       for(int num:map.keySet()){
+           if(num>max_key){
+               max_key=num;
+             if(max_key==map.get(max_key))returning=max_key;  
+           }
        }
-        return -1;
+       
+        return returning!=0?returning:-1;
+
+
+
+
+
+    //     HashMap<Integer,Integer>map=new HashMap();
+    //     Arrays.sort(arr);
+    //     for(int i=0;i<arr.length;i++){
+    //         map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+    //     }
+    //    System.out.print(map);
+    //    for(int i=arr.length-1;i>=0;i--){
+    //        if(arr[i]==map.get(arr[i]))return arr[i];
+    //    }
+    //     return -1;
     }
 }
