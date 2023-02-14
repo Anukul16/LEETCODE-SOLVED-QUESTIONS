a7029ecfd9592dfa68a9d97a8570650dfdd7bfda
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-02-14 08:32:40 +0530

    Added solution - LeetHub

--- Walls Coloring - GFG/walls-coloring.java
+++ Walls Coloring - GFG/walls-coloring.java
@@ -32,15 +32,18 @@ class GFG{
 
 class Solution{
     int minCost(int [][] colors, int N){
-        int[][] dp = new int[N][3];
+        int[] prevMinCosts = new int[3];
         for (int i = 0; i < 3; i++) {
-            dp[0][i] = colors[0][i];
+            prevMinCosts[i] = colors[0][i];
         }
         for (int i = 1; i < N; i++) {
-            dp[i][0] = colors[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
-            dp[i][1] = colors[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] = colors[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+            int[] currMinCosts = new int[3];
+            currMinCosts[0] = colors[i][0] + Math.min(prevMinCosts[1], prevMinCosts[2]);
+            currMinCosts[1] = colors[i][1] + Math.min(prevMinCosts[0], prevMinCosts[2]);
+            currMinCosts[2] = colors[i][2] + Math.min(prevMinCosts[0], prevMinCosts[1]);
+            prevMinCosts = currMinCosts;
         }
-        return Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2]));
+        return Math.min(prevMinCosts[0], Math.min(prevMinCosts[1], prevMinCosts[2]));
+    
     }
 }
