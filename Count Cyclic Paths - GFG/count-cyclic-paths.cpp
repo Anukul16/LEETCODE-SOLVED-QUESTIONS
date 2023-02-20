737505d578be7b887d7253f559940d84ec63576b
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-02-20 10:39:28 +0530

    Added solution - LeetHub

---
+++ Count Cyclic Paths - GFG/count-cyclic-paths.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+int dp[1000010][4];
+class Solution{   
+public:
+    int countPaths(int N){
+        const int mod = 1e9 + 7;
+        
+        for(int i = 0; i < 4; i++) dp[0][i] = 0;
+        dp[0][0] = 1;
+        
+        for(int i = 1; i < N + 1; i++){
+            for(int j = 0; j < 4; j++){
+                int x = (j + 1) % 4;
+                int y = (j - 1 + 4) % 4;
+                int z = (j + 6) % 4;
+                
+                dp[i][j] = (dp[i - 1][x] + dp[i - 1][y]) % mod;
+                dp[i][j] = (dp[i][j] + dp[i - 1][z]) % mod;
+            }
+        }
+        
+        return dp[N][0];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        
+        Solution ob;
+        cout << ob.countPaths(N) << endl;
+    }
+    return 0; 
+}
+
+// } Driver Code Ends
