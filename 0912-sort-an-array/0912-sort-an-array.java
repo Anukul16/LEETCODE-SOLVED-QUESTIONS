52d2da2bbf6eb18c8c0805ae66cee1552fbd768f
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-03-01 06:15:01 +0530

    Time: 102 ms (26.77%), Space: 51.9 MB (84.65%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int res[]=new int[nums.length];
-        Queue<Integer>pq=new PriorityQueue();
-        for(int num:nums)pq.offer(num);
-        for(int i=0;i<nums.length;i++){
-            res[i]=pq.poll();
-        }
-        return res;
+        Queue<Integer>pq=new PriorityQueue<>();
+        for(int x:nums)pq.offer(x);
+        int idx=0;
+        while(!pq.isEmpty())nums[idx++]=pq.poll();
+        return nums;
     }
 }
