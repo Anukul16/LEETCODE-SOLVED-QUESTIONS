71c805c22f7f3dc915a7fe6971cb7d411abfe434
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-03-12 19:11:15 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (100.00%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -1,23 +1,16 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
+       
         int ans=0;
-        while(left<=right){
-            if(isVowel(words[left]))ans++;
-            left++;
+        for(int i=left;i<=right;i++)
+        {
+            char ch1=words[i].charAt(0);
+            char ch2=words[i].charAt(words[i].length()-1);
+            if((ch1=='a'||ch1=='e'||ch1=='i'||ch1=='o'||ch1=='u') && (ch2=='a'||ch2=='e'||ch2=='i'||ch2=='o'||ch2=='u'))
+            {
+                ans++;
+            }
         }
         return ans;
-    }
-    private boolean isVowel(String s){
-        char ch[]=s.toCharArray();
-        int n=ch.length;
-        if(n==1 &&(ch[0]=='a'||ch[0]=='e'||ch[0]=='i'||ch[0]=='o'||ch[0]=='u')){
-            return true;
-        }
-        char ch1=ch[0],ch2=ch[n-1];
-        if((ch1=='a'||ch1=='e'||ch1=='i'||ch1=='o'||ch1=='u') && (ch2=='a'||ch2=='e'||ch2=='i'||ch2=='o'||ch2=='u')){
-            return true;
-        }
-        return false;
-        
     }
 }
