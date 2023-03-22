d261f52f28fc01e5740ebfc98548ba7dd21e9b4a
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-03-22 19:54:20 +0530

    Time: 1 ms (98.67%), Space: 40.8 MB (23.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            
+            switch(ch){
+                case'(':
+                case'{':
+                case'[':
+                    stack.push(ch);
+                    break;
+                case')':
+                    if(stack.isEmpty() || stack.pop()!='('){
+                        return false;
+                    }
+                    break;
+                case'}':
+                    if(stack.isEmpty() || stack.pop()!='{'){
+                        return false;
+                    }
+                    break;
+                case']':
+                    if(stack.isEmpty() || stack.pop()!='['){
+                        return false;
+                    }
+                    break;
+            }  
+                
+        }
+        return stack.isEmpty();
+    }
+}
