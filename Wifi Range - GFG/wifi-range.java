0aeea8a2e6588a39a93915a568a895c732d34eb7
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-04-19 23:22:17 +0530

    Added solution - LeetHub

---
+++ Wifi Range - GFG/wifi-range.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int X = Integer.parseInt(input_line[1]);
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            boolean ans = ob.wifiRange(N, S, X); 
+            if(ans)
+            System.out.println(1);
+            else
+            System.out.println(0);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    boolean wifiRange(int N, String S, int X) 
+    { 
+        // code here
+        int A[] = new int[N]; Arrays.fill(A,100009);
+       
+        int B[] = new int[N]; Arrays.fill(B,100009);
+        
+        int cur = 100009;
+        for(int i = 0; i < N; i++)
+        {
+            if(S.charAt(i) == '1')
+                cur = i;
+            A[i] = cur;
+        }
+        cur = 100009;
+        for(int i = N - 1; i >= 0; i--)
+        {
+            if(S.charAt(i) == '1')
+                cur = i;
+            B[i] = cur;
+        }
+        //condition to check if any room is not reachable
+        for(int i = 0; i < N; i++){
+            if(Math.abs(i - A[i]) > X && Math.abs(i - B[i]) > X){
+                return false;
+            }
+        }
+        return true;
+    }
+} 
