4ae94114c46d57c59e97a1fef1ea1034004bd198
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-05-03 12:38:32 +0530

    Added solution - LeetHub

---
+++ Make Palindrome - GFG/make-palindrome.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class StringArray
+{
+    public static String[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        return s;
+    }
+
+    public static void print(String[] a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<String> a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            String[] arr = StringArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.makePalindrome(n, arr);
+            
+            String _result_val = (res) ? "YES" : "NO";
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean makePalindrome(int n, String[] arr) {
+        // code here
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            map.put(arr[i] , map.getOrDefault(arr[i],0) + 1);
+        }
+        
+        for(int i = 0; i < n; i++){
+            String str = arr[i];
+            String reverse = new StringBuilder(str).reverse().toString();
+            if(!map.get(str).equals(map.get(reverse))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+        
