91fb9475a323d1baa5b0fc9ea229c3e999d1967e
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-05-23 18:41:30 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    // just do a dry run will get over it
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+       boolean[]visited=new boolean[V];
+       for(int i=0;i<V;i++){
+           if(visited[i]==false){
+               if(dfs(i,-1,visited,adj)==true)return true;
+           }
+       }
+       return false;
+    }
+    private boolean dfs(int node,int parent,boolean[]visited,ArrayList<ArrayList<Integer>> adj){
+        visited[node]=true;
+        for(int it:adj.get(node)){
+            if(visited[it]==false){
+                if(dfs(it,node,visited,adj)==true)return true;
+            }else if(it!=parent)return true;
+        }
+        return false;
+    }
+   
+}
