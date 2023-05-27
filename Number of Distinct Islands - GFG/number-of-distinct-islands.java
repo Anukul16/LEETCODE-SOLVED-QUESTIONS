54d048acedea3de2cd5a313e7e74cf3e81ef3621
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-05-28 00:04:40 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.java
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.java
@@ -36,48 +36,36 @@ class GFG {
 
 class Solution {
     // Depth-first search (DFS) method
-    void dfs(int row, int col, int[][] visited, int[][] grid, ArrayList<String> list, int row0, int col0) {
-        // Base case and boundary conditions
-        if (row < 0 || row == grid.length || col < 0 || col == grid[0].length || grid[row][col] == 0 || visited[row][col] == 1)
+    private void dfs(int row,int col,int[][]visited,int[][]grid,int row0,int col0,ArrayList<String>list){
+        if(row<0 || row==grid.length || col<0 || col==grid[0].length || visited[row][col]==1 || grid[row][col]==0){
             return;
-
-        // Mark current cell as visited
-        visited[row][col] = 1;
-
-        // Add the relative coordinates of the current cell to the list
-        list.add(toString(row - row0, col - col0));
-
-        // Recursively visit the neighboring cells
-        dfs(row - 1, col, visited, grid, list, row0, col0); // Up
-        dfs(row, col + 1, visited, grid, list, row0, col0); // Right
-        dfs(row + 1, col, visited, grid, list, row0, col0); // Down
-        dfs(row, col - 1, visited, grid, list, row0, col0); // Left
+        }
+        visited[row][col]=1;
+        list.add(toString(row-row0,col-col0));
+        
+        dfs(row-1,col,visited,grid,row0,col0,list);
+        dfs(row,col+1,visited,grid,row0,col0,list);
+        dfs(row+1,col,visited,grid,row0,col0,list);
+        dfs(row,col-1,visited,grid,row0,col0,list);
     }
-
-    // Convert relative coordinates to string representation
-    private String toString(int r, int c) {
-        return Integer.toString(r) + " " + Integer.toString(c);
+    private String toString(int r,int c){
+        return (Integer.toString(r)+" "+Integer.toString(c));
     }
 
     public int countDistinctIslands(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
-        int[][] visited = new int[n][m];
-        HashSet<ArrayList<String>> set = new HashSet<>();
-
-        // Iterate over each cell in the grid
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                // If the cell is land and not visited, initiate DFS to explore the island
-                if (grid[i][j] == 1 && visited[i][j] == 0) {
-                    ArrayList<String> list = new ArrayList<>();
-                    dfs(i, j, visited, grid, list, i, j);
+        int m=grid[0].length;
+        int[][]visited=new int[n][m];
+        HashSet<ArrayList<String>>set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0 && grid[i][j]==1){
+                    ArrayList<String>list=new ArrayList<>();
+                    dfs(i,j,visited,grid,i,j,list);
                     set.add(list);
                 }
             }
         }
-
-        // Return the number of distinct islands
         return set.size();
     }
 }
