cd8ef4fec77c133544d1dc3fe7bcf00cfcb1aef3
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-06-17 17:14:03 +0530

    Added solution - LeetHub

---
+++ Geek Jump - GFG/geek-jump.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int N=sc.nextInt();
+            int []arr=new int[N];
+            for(int i=0;i<N;i++){
+                arr[i]=sc.nextInt();
+                
+            }
+            Solution obj=new Solution();
+            int res=obj.minimumEnergy(arr,N);
+            System.out.println(res);
+            
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int minimumEnergy(int arr[],int n){
+        //code here
+         int dp[]=new int[n];
+        dp[0]=0;
+        for(int i=1;i<=n-1;i++){
+            int first=dp[i-1]+Math.abs(arr[i]-arr[i-1]);
+            int second=Integer.MAX_VALUE;
+            if(i>1)second=dp[i-2]+Math.abs(arr[i]-arr[i-2]);
+            dp[i]=Math.min(first,second);
+        }
+        return dp[n-1];
+    }
+}
