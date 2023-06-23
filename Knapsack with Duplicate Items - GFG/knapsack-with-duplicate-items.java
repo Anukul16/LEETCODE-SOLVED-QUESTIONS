1af81267e7072001f24d7b2e7293999ba2709742
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-06-23 17:04:02 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Integer[][] dp;
+
+    static int knapSack(int N, int W, int val[], int wt[]) {
+        dp = new Integer[N + 1][W + 1];
+        return helper(N, W, val, wt);
+    }
+
+    static int helper(int N, int W, int val[], int wt[]) {
+        if (N == 0 || W == 0)
+            return 0;
+
+        if (dp[N][W] != null)
+            return dp[N][W];
+
+        int max = -1;
+        if (wt[N - 1] <= W) {
+            int pick = val[N - 1] + helper(N, W - wt[N - 1], val, wt);
+            int notPick = helper(N - 1, W, val, wt);
+            max = Math.max(pick, notPick);
+        } else {
+            max = helper(N - 1, W, val, wt);
+        }
+
+        dp[N][W] = max;
+        return max;
+    }
+}
