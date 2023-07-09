6956ec341a5c51774e571c3bfa426c5c0a4373d2
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-07-09 09:57:56 +0530

    Added solution - LeetHub

---
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        Set<Integer>set=new HashSet<>();
+        int max=Integer.MIN_VALUE;
+        for(int num:arr){
+            set.add(num);
+            max=Math.max(num,max);
+        }
+        if(max<0)return 1;
+        for(int i=1;i<=max;i++){
+            if(!set.contains(i))return i;
+        }
+        return max+1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) 
+    {
+
+		Scanner sc=new Scanner(System.in);
+		
+        //taking testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //input number n
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=sc.nextInt();
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			System.out.println(missing);
+		}
+    }
+}
+
+
+// } Driver Code Ends
