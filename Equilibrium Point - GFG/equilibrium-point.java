0a6ce95f57e61d9837d082711c915b8b80aa0a00
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2023-09-23 09:29:14 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        long pSum[]=new long[n+1];
+        pSum[0]=0;
+        for(int i=1;i<=n;i++){
+            pSum[i]=arr[i-1]+pSum[i-1];
+        }
+        int ans=-1;
+        for(int i=1;i<pSum.length;i++){
+            if(pSum[i-1]==pSum[pSum.length-1]-pSum[i]){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
