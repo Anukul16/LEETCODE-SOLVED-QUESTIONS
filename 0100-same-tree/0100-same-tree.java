ab1f2a91d2746561bb41ea3d406c4102d3a09f21
Anukul Maity <102921169+Anukul16@users.noreply.github.com>
2024-02-26 10:55:42 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (69.39%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,38 +1,10 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     String val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        ArrayList<String>plist=new ArrayList<>();
-        ArrayList<String>qlist=new ArrayList<>();
-        Preorder(p,plist);
-        Preorder(q,qlist);
-        if(plist.size()!=qlist.size())return false;
-        for(int i=0;i<plist.size();i++){
-            if(!plist.get(i).equals(qlist.get(i)))return false;
-        }
-        return true;
+        if(p==null && q==null)return true;
+        if(p==null || q==null)return false;
 
-    }
-    private void Preorder(TreeNode root,List<String>list){
-        if(root==null){
-            list.add("null");
-            return;
-        }
-        list.add(String.valueOf(root.val));
-        Preorder(root.left,list);
-        Preorder(root.right,list);
+        if(p.val!=q.val)return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
     }
 }
